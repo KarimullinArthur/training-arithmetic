f37d60c2fe7d107b3c04e131229f6c0cce811767
Arthur <51211371+KarimullinArthur@users.noreply.github.com>
2021-09-16 19:23:38 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import random
 from colorama import Fore, Style
 
-while True==True:
-    a = random.randint(1,100)
-    b = random.randint(1,100)
-    c = a + b
+time = 5
 
-    print(a,"+",b,"= ", end='')
-    result = input()
+def sumEasy(): 
+    for x in range(time):
+        a = random.randint(1,100)
+        b = random.randint(1,100)
+        c = a + b
 
-    if result == "exit":
-        break
+        print(a,"+",b,"= ", end='')
+        result = input()
 
-    result = int(result)
+        if result == "exit":
+            break
 
-    if result == c:
-        print(Fore.GREEN + 'Right',Style.RESET_ALL)
+        result = int(result)
 
-    else :
-        print(Fore.RED + 'Wrong',Style.RESET_ALL, a,"+",b,"=",c)
+        if result == c:
+            print(Fore.GREEN + 'Right',Style.RESET_ALL)
+
+        else :
+            print(Fore.RED + 'Wrong',Style.RESET_ALL, a,"+",b,"=",c)
+
+    with open('stat.txt', 'r+') as f: #r+ does the work of rw
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            if line.startswith('lose'):
+                lines[i] = lines[i].strip() + '2012\n'
+        f.seek(0)
+        for line in lines:
+            f.write(line)
+        open('stat.txt', 'r+').close()
+
+sumEasy()
