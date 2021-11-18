2b2cc6f43e7edaa91652a9cc2a582ccd6f2d432b
Arthur <arthurkarimullin2005@gmail.com>
2021-11-18 15:55:05 +0500

    add "minus" and some

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
 import random
 from colorama import Fore, Style
+import operator
 
-while True==True:
-    a = random.randint(1,100)
-    b = random.randint(1,100)
-    c = a + b
+ops = { "+": operator.add, "-": operator.sub }
 
-    print(a,"+",b,"= ", end='')
-    result = input()
 
-    if result == "exit":
-        break
+def main(mode):
+	while True==True:
+		a = random.randint(1,100)
+		b = random.randint(1,100)
+		c = ops[mode](a,b)
 
-    result = int(result)
+		print(a,mode,b,"= ", end='')
+		result = input()
 
-    if result == c:
-        print(Fore.GREEN + 'Right',Style.RESET_ALL)
+		if result == "exit":
+			break
 
-    else :
-        print(Fore.RED + 'Wrong',Style.RESET_ALL, a,"+",b,"=",c)
+		result = int(result)
+
+		if result == c:
+			print(Fore.GREEN + 'Right',Style.RESET_ALL)
+
+		else :
+			print(Fore.RED + 'Wrong',Style.RESET_ALL, a,mode,b,"=",c)
+
+
+def menu():
+	print("Hello")
+	print("+ |sum","- |subtraction",sep='\n')
+	
+	mode = input()
+	
+	if mode == "+":
+		main("+")
+	if mode == "-":
+		main("-")
+	
+	
+menu()
+
